58f0ee6abaaa99762e1e7d7d412532f239bf3251
Likhit Garimella <likhitgmella@gmail.com>
2024-05-24 20:36:40 -0400

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Likhit Garimella on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
