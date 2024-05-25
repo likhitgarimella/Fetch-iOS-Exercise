a54c5a2a5e19701da4435f8be5ec44e364c5a5c3
Likhit Garimella <likhitgmella@gmail.com>
2024-05-25 19:28:53 -0400

    models comments

--- Fetch/Models.swift
+++ Fetch/Models.swift
@@ -8,35 +8,45 @@
 import Foundation
 
 struct Meal: Identifiable, Codable {
+    // meal id
     let idMeal: String
+    // meal name
     let strMeal: String
     
+    // identifier from idMeal
     var id: String { idMeal }
 }
 
 struct MealResponse: Codable {
+    // array of Meal objects
     let meals: [Meal]
 }
 
 struct MealDetailResponse: Codable {
+    // array of MealDetail objects
     let meals: [MealDetail]
 }
 
 struct MealDetail: Codable {
     let strMeal: String
+    // addl params
     let strInstructions: String
+    // array of Ingredient structs...
     let ingredients: [Ingredient]
     
+    // inside Ingredient...
     struct Ingredient: Codable {
         let name: String
         let measure: String
     }
-
+    
+    // decode json
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         strMeal = try container.decode(String.self, forKey: .strMeal)
         strInstructions = try container.decode(String.self, forKey: .strInstructions)
         
+        // looping over possible ingredient and measure pairs up to 20 and creating Ingredient objects
         var ingredients: [Ingredient] = []
         for i in 1...20 {
             if let nameKey = CodingKeys(stringValue: "strIngredient\(i)"),
