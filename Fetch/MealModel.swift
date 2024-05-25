1798495817460c62f462197c65daff6d4722b81d
Likhit Garimella <likhitgmella@gmail.com>
2024-05-25 17:11:27 -0400

    first commit

---
+++ Fetch/MealModel.swift
@@ -0,0 +1,47 @@
+//
+//  Meal.swift
+//  Fetch
+//
+//  Created by Likhit Garimella on 5/24/24.
+//
+
+import Foundation
+
+struct Meal: Identifiable, Codable {
+    let idMeal: String
+    let strMeal: String
+    
+    var id: String { idMeal }
+}
+
+struct MealResponse: Codable {
+    let meals: [Meal]
+}
+
+struct MealDetail: Codable {
+    let strMeal: String
+    let strInstructions: String
+    let ingredients: [Ingredient]
+    
+    struct Ingredient: Codable {
+        let name: String
+        let measure: String
+    }
+    
+    static func from(json: [String: Any]) -> MealDetail? {
+        guard let strMeal = json["strMeal"] as? String,
+              let strInstructions = json["strInstructions"] as? String else {
+            return nil
+        }
+        
+        var ingredients: [Ingredient] = []
+        for i in 1...20 {
+            if let name = json["strIngredient\(i)"] as? String, !name.isEmpty,
+               let measure = json["strMeasure\(i)"] as? String, !measure.isEmpty {
+                ingredients.append(Ingredient(name: name, measure: measure))
+            }
+        }
+        
+        return MealDetail(strMeal: strMeal, strInstructions: strInstructions, ingredients: ingredients)
+    }
+}
---
+++ Fetch/MealViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MealViewModel.swift
+//  Fetch
+//
+//  Created by Likhit Garimella on 5/24/24.
+//
+
+import Foundation
